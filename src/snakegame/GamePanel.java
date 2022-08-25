689b1226b51d83addc68f2fe74fecc0cba0682f9
OlgaVasileva <polubaryeva.olga@gmail.com>
2022-08-25 14:53:39 -0400

    comments were addded to snake game project

--- src/snakegame/GamePanel.java
+++ src/snakegame/GamePanel.java
@@ -48,7 +48,7 @@ public class GamePanel extends JPanel implements ActionListener{
     
     private void draw(Graphics g){
         if (running){
-            /*
+            /* draw a grid
             for (int i = 0 ; i < SCREEN_HEIGHT/UNIT_SIZE; i++){
                 g.drawLine(i*UNIT_SIZE,0,i*UNIT_SIZE,SCREEN_HEIGHT);
                 g.drawLine(0, i*UNIT_SIZE,SCREEN_WIDTH,i*UNIT_SIZE);
@@ -64,6 +64,7 @@ public class GamePanel extends JPanel implements ActionListener{
 	}
 	else {
 	   g.setColor(new Color(45,180,0));
+           // colorful snake
 	   //g.setColor(new Color(random.nextInt(255),random.nextInt(255),random.nextInt(255)));
 	   g.fillRect(x[i], y[i], UNIT_SIZE, UNIT_SIZE);
 	   }			
